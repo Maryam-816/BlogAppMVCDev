3706b6b15ba879dd223e40598da390d74ddcff25
Maryam-816 <rustamova.maryam2009@gmail.com>
2021-03-14 12:56:33 +0400

    Login page for Admin added

---
+++ BlogMVCApp/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using BlogMVCApp.Areas.Admin.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogMVCApp.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Admin/Account
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel login)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View();
+        }
+    }
+}
